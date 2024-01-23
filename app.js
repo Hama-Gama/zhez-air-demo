a94ab8d2b481db23219f7e04e6b09df13b05c532
Hama-Gama <115399990+Hama-Gama@users.noreply.github.com>
2024-01-23 07:45:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+//  header change background on scroll
+ $(function() {
+	$(window).on("scroll", function() {
+			if($(window).scrollTop() > 50) {
+					$(".header").addClass("active");
+			} else {
+					//remove the background property so it comes transparent again (defined in your css)
+				 $(".header").removeClass("active");
+			}
+	});
+});
+
+
+
 // background ibg class
 function ibg(){
    let ibg=document.querySelectorAll(".ibg"); 
@@ -17,18 +31,39 @@ function ibg(){
  })
  
 
-//  header change background on scroll
- $(function() {
-	$(window).on("scroll", function() {
-			if($(window).scrollTop() > 50) {
-					$(".header").addClass("active");
-			} else {
-					//remove the background property so it comes transparent again (defined in your css)
-				 $(".header").removeClass("active");
-			}
-	});
-});
 
+// testimonial slider
+var swiper = new Swiper(".slide-content", {
+	slidesPerView: 3,
+	spaceBetween: 25,
+	loop: true,
+	centerSlide: 'true',
+	fade: 'true',
+	grabCursor: 'true',
+	autoplay: {
+		 delay: 2000,
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		dynamicBullets: true,
+		clickable: true,
+	},
+	navigation: {
+		 nextEl: ".swiper-button-next",
+		 prevEl: ".swiper-button-prev",
+	},
+	breakpoints:{
+		 0: {
+				slidesPerView: 1,
+		 },
+		 767: {
+				slidesPerView: 2,
+		 },
+		 1200: {
+				slidesPerView: 3,
+		 },
+	},
+});
 
 
 
